d95ca236f0e9f90f8ccdea667eb61895735de1f2
Timofey Petrikevich <petrik3003@gmail.com>
2023-02-01 23:41:00 +0000

    feat: refactor + status

--- src/App.js
+++ src/App.js
@@ -2,20 +2,54 @@ import React, { useState } from 'react';
 import Board from './Board';
 
 export default function Game() {
-  const [nextIsX, setNextIsX] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
 
-  const currentSquares = history[history.length - 1];
+  const nextIsX = (currentMove % 2 === 0);
+  const currentSquares = history[currentMove];
 
   const handlePlay = (nextSquares) => {
-    setHistory([...history, nextSquares]);
-    setNextIsX(!nextIsX);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
+
+  const jumpTo = (move) => {
+    setCurrentMove(move);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+
+    let moveDescription;
+    if (move == currentMove) {
+      moveDescription = `You're at move #${currentMove}`;
+    } else {
+      moveDescription = (
+        <button onClick={() => jumpTo(move)}>
+          {description}
+        </button>
+      )
+    }
+
+    return (
+      <li key={move}>
+        {moveDescription}
+      </li>
+    );
+  });
 
   return (
     <div className="game">
       <div>
         <Board
+          boardWidth={3}
+          boardHeight={3}
           className="game-board"
           nextIsX={nextIsX}
           squares={currentSquares}
@@ -23,7 +57,7 @@ export default function Game() {
         />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
